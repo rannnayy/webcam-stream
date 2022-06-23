4b65f041162e5d66674a4006881b049968d2a389
Maharani Ayu Putri Irawan <72875641+rannnayy@users.noreply.github.com>
2022-06-23 12:27:21 +0700

    adjust lines

--- webcamstream.py
+++ webcamstream.py
@@ -6,7 +6,7 @@ vc = cv2.VideoCapture(0)
 if vc.isOpened():
     rval, frame = vc.read()
     # print(frame.shape)
-    cv2.line(frame, (int(frame.shape[1]/2), 0),(int(frame.shape[1]/2), frame.shape[0]), (255, 0, 0), 10, 1)
+    cv2.line(frame, (int(frame.shape[1]/2), 0), (int(frame.shape[1]/2), frame.shape[0]), (0, 0, 255), 2, 1)
 else:
     rval = False
 
@@ -17,8 +17,10 @@ while rval:
     if key == 27: # exit on ESC
         break
     else:
-        cv2.line(frame, (int(frame.shape[1]/2), 0),(int(frame.shape[1]/2), frame.shape[0]), (255, 0, 0), 10, 1)
-        cv2.line(frame, (0, int(frame.shape[0]/2)),(frame.shape[1], int(frame.shape[0]/2)), (255, 0, 0), 10, 1)
+        # Vertical Line
+        cv2.line(frame, (int(frame.shape[1]/2), 0), (int(frame.shape[1]/2), frame.shape[0]), (0, 0, 255), 2, 1)
+        # Horizontal Line
+        cv2.line(frame, (int(frame.shape[0]/3), int(frame.shape[0]/2)), (int(frame.shape[0]), int(frame.shape[0]/2)), (0, 0, 255), 2, 1)
 
 vc.release()
 cv2.destroyWindow("Camera")
